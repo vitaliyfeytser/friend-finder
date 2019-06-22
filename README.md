ecee3554a0a2170da473356fbc170c8b83685c75
VitaliyFeytser <vitaliyfeytser@gmail.com>
2019-06-21 19:55:12 -0700

    before heroku

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
-# friend-finder
+# Friend-Finder
+
+A compatibility-based "FriendFinder" application -- basically a dating app. This full-stack site will
+take in results your users' surveys, then compare their answers with those from other users. The app 
+will then display the name and picture of the user with the best overall match.
+Preview Site
+https://friend-finder-donotunplug.herokuapp.com
+
+Built With
+* Node.JS
+* Express
+* JavaScript
+* JQuery
+* Bootstrap
+* HTML
+* CSS
---
+++ app/data/friends.js
@@ -0,0 +1,35 @@
+//create array of default friends
+
+var friends = [{
+    name: "Bobby John",
+    photo: "https://static.pexels.com/photos/343717/pexels-photo-343717.jpeg",
+    scores: [3, 1, 4, 4, 2, 4, 5, 2, 4, 2]
+  }, {
+    name: "Susie Schmidt",
+    photo: "https://static.pexels.com/photos/206481/pexels-photo-206481.jpeg",
+    scores: [2, 5, 3, 4, 5, 3, 1, 3, 2, 4]
+  }, {
+    name: "Ron Burgundy",
+    photo: "https://static.pexels.com/photos/249760/pexels-photo-249760.jpeg",
+    scores: [4, 3, 3, 2, 1, 2, 5, 4, 2, 1]
+  },{
+    name: "Nate Jade",
+    photo: "https://static.pexels.com/photos/270308/pexels-photo-270308.jpeg",
+    scores: [1, 2, 3, 3, 4, 1, 2, 5, 3, 3]
+  }, {
+    name: "Don Jr",
+    photo: "https://static.pexels.com/photos/249757/pexels-photo-249757.jpeg",
+    scores: [5, 4, 5, 3, 2, 4, 2, 1, 3, 2]
+  }, {
+    name: "Mary Joe",
+    photo: "https://static.pexels.com/photos/388517/pexels-photo-388517.jpeg",
+    scores: [2, 1, 1, 2, 5, 4, 3, 3, 1, 4]
+  }, {
+    name: "Suzie Mole",
+    photo: "https://static.pexels.com/photos/64385/pexels-photo-64385.jpeg",
+    scores: [2, 2, 1, 3, 4, 2, 5, 3, 3, 4]
+  }];
+  
+   //export array
+  
+  module.exports = friends;
